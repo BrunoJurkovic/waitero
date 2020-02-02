7a3de59350a9996b6760651bfd1b7ecec6c72aa7
Benjamin <theslimeslayer04@gmail.com>
2020-02-02 08:18:17 -0500

    Initialized cart and worked on order page

--- waiteroclient/lib/router/router.dart
+++ waiteroclient/lib/router/router.dart
@@ -1,6 +1,8 @@
 import 'package:auto_route/auto_route_annotations.dart';
+import 'package:waiteroclient/screens/order/order.dart';
 
 @autoRouter
 class $Router {
-  
+  @initial
+  OrderPage orderPage;
 }
--- waiteroclient/lib/screens/cart/cart.dart
+++ waiteroclient/lib/screens/cart/cart.dart
@@ -2,20 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 import 'package:waiteroclient/components/scaffold/custom_scaffold.dart';
 import 'package:waiteroclient/components/unsupported_platform/unsupported_platform.dart';
-import 'package:waiteroclient/screens/order/index.dart';
+import 'package:waiteroclient/screens/cart/mobile/cart_mobile.dart';
+import 'package:waiteroclient/screens/cart/tablet/cart_tablet.dart';
 
-class OrderPage extends StatefulWidget {
-  const OrderPage({Key key}) : super(key: key);
-
-  @override
-  _OrderPageState createState() => _OrderPageState();
-}
-
-class _OrderPageState extends State<OrderPage> {
-  @override
-  void initState() {
-    super.initState();
-  }
+class CartPage extends StatelessWidget {
+  const CartPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -24,16 +15,15 @@ class _OrderPageState extends State<OrderPage> {
         builder: (BuildContext context, SizingInformation sizing) {
           switch (sizing.deviceScreenType) {
             case DeviceScreenType.Mobile:
-              return const OrderPageMobile();
+              return const CartPageMobile();
             case DeviceScreenType.Tablet:
-              return const OrderPageTablet();
+              return const CartPageTablet();
             case DeviceScreenType.Desktop:
-              return const OrderPageDesktop();
+              return const CartPageTablet();
             default:
               return const UnsupportedPlatform();
           }
         },
-        breakpoints: ScreenBreakpoints(desktop: 1023, tablet: 768, watch: null),
       ),
     );
   }
